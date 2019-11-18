21b91eae9fe59969a503de1d9787b61ca846411e
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-11-18 21:21:53 +0530

    Remove redundant code from onNavigationItemSelected()

--- app/src/main/java/com/vinithepooh/notifier/MainActivity.java
+++ app/src/main/java/com/vinithepooh/notifier/MainActivity.java
@@ -652,23 +652,7 @@ public class MainActivity extends AppCompatActivity
                         e.getMessage());
             }
         }
-
-            /**
-            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-
-            Menu menu = navigationView.getMenu();
-            menuItem = menu.add("My new menu");
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-            */
-
+        
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
